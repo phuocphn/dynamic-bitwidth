8e96721afacfe86898f700a099f9ac00ba2e9f32
phuocphn <phuocphn@gmail.com>
2021-01-22 11:43:05 +0900

    add activation

--- quantizer/condlsq.py
+++ quantizer/condlsq.py
@@ -62,6 +62,63 @@ class attention2d(nn.Module):
         return gradient_approximation(x, self.temperature), x
 
 
+class ActivationQuantizer(torch.nn.Module):
+    def __init__(self, bit, K, is_activation=False):
+        super(ActivationQuantizer,self).__init__()
+
+        self.alpha = nn.Parameter(torch.randn(K, 1))
+        self.bit = list(np.array(range(K)) + 2)
+        self.K = K
+        self.is_activation = is_activation
+        self.register_buffer('init_state', torch.zeros(1))
+                
+        if is_activation:
+            # print ("Un-expected")
+            self.Qns = []
+            for bit in np.array(range(K)) + 2:
+                self.Qns.append(0)
+            self.register_buffer('Qns', torch.tensor(self.Qns, requires_grad=False).view(-1, 1))
+
+            self.Qps = []
+            for bit in np.array(range(K)) + 2:
+                self.Qps.append(2** bit -1)
+            # self.Qps = torch.tensor(self.Qps, requires_grad=False).view(-1, 1)
+            self.register_buffer('Qps', torch.tensor(self.Qps, requires_grad=False).view(-1, 1))
+
+
+        # else:
+        #     self.Qns = []
+        #     for bit in np.array(range(K)) + 2:
+        #         self.Qns.append(-2 ** (bit- 1))
+        #     self.Qns = torch.tensor(self.Qns, requires_grad=False).view(-1, 1)
+
+        #     self.Qps = []
+        #     for bit in np.array(range(K)) + 2:
+        #         self.Qps.append(2 ** (bit-1) -1)
+        #     self.Qps = torch.tensor(self.Qps, requires_grad=False).view(-1, 1)
+
+    def forward(self, x, attention):
+        if self.training and self.init_state == 0:
+            self.alpha.data.copy_( (2* x.detach().abs().mean() / math.sqrt(self.Qps)))
+            self.init_state.fill_(1)
+            print (self.__class__.__name__, "Initializing step-size value ...")
+        
+        runtime_Qns = torch.mm(attention, self.Qns)
+        runtime_Qps = torch.mm(attention, self.Qps)
+        runtime_alphas = torch.mm(attention, self.alpha)
+
+
+
+
+        g = 1.0 / ( (x.numel() / x.size(0)) * runtime_Qps) ** 0.5
+        _alpha = grad_scale(self.alpha, g)
+        clipped = torch.max(torch.min(x/_alpha, runtime_Qps), runtime_Qns)
+        x_q = round_pass(clipped) * _alpha
+        return x_q
+
+    def __repr__(self):
+        return "LSQQuantizer (bit=%s, is_activation=%s)" % (self.bit, self.is_activation)
+
 
 class LSQQuantizer(torch.nn.Module):
     def __init__(self, bit, K, is_activation=False):
@@ -74,15 +131,16 @@ class LSQQuantizer(torch.nn.Module):
         self.register_buffer('init_state', torch.zeros(1))
                 
         if is_activation:
-            self.Qns = []
-            for bit in np.array(range(K)) + 2:
-                self.Qns.append(0)
-            self.Qns = torch.tensor(self.Qns, requires_grad=False).view(-1, 1)
+            print ("Un-expected")
+            # self.Qns = []
+            # for bit in np.array(range(K)) + 2:
+            #     self.Qns.append(0)
+            # self.Qns = torch.tensor(self.Qns, requires_grad=False).view(-1, 1)
 
-            self.Qps = []
-            for bit in np.array(range(K)) + 2:
-                self.Qps.append(2** bit -1)
-            self.Qps = torch.tensor(self.Qps, requires_grad=False).view(-1, 1)
+            # self.Qps = []
+            # for bit in np.array(range(K)) + 2:
+            #     self.Qps.append(2** bit -1)
+            # self.Qps = torch.tensor(self.Qps, requires_grad=False).view(-1, 1)
 
         else:
             self.Qns = []
@@ -97,13 +155,13 @@ class LSQQuantizer(torch.nn.Module):
 
     def forward(self, x):
         if self.training and self.init_state == 0:
-            self.alpha.data.copy_( (2* x.detach().abs().mean() / math.sqrt(self.Qps)))
+            self.alpha.data.copy_( (2* x.detach().abs().mean() / (self.Qps.to(x.device))**0.5))
             self.init_state.fill_(1)
             print (self.__class__.__name__, "Initializing step-size value ...")
         
-        g = 1.0 / math.sqrt(x.numel() * self.Qps)
+        g = 1.0 / (x.numel() * self.Qps.to(x.device)) ** 0.5
         _alpha = grad_scale(self.alpha, g)
-        clipped = torch.max(torch.min(x/_alpha, self.Qps), self.Qns)
+        clipped = torch.max(torch.min(x/_alpha, self.Qps.to(x.device)), self.Qns.to(x.device))
         x_q = round_pass(clipped) * _alpha
         return x_q
 
@@ -128,7 +186,7 @@ class Dynamic_LSQConv2d(nn.Module):
         self.K = K
         self.attention = attention2d(in_channels, ratio, K, temperature)
         self.quan_w = LSQQuantizer(bit=4, K=K, is_activation=False)
-        self.quan_a = LSQQuantizer(bit=4, K=K, is_activation=True)
+        self.quan_a = ActivationQuantizer(bit=4, K=K, is_activation=True)
 
         self.weight = nn.Parameter(torch.randn(K, out_channels, in_channels//groups, kernel_size[0], kernel_size[0]), requires_grad=True)
         if bias:
@@ -150,9 +208,14 @@ class Dynamic_LSQConv2d(nn.Module):
     def forward(self, x):#
         softmax_attention, raw_attention = self.attention(x)
         batch_size, in_channels, height, width = x.size()
+        x = self.quan_a(x, softmax_attention)
+
         x = x.view(1, -1, height, width)
-        _x = x.unsqueeze(0).repeat(self.K, 1,1,1,1).view(self.K, -1)
-        _x = self.quan_a(_x)
+        
+
+
+        # _x = x.unsqueeze(0).repeat(self.K, 1,1,1,1).view(self.K, -1)
+        # _x = self.quan_a(_x)
 
 
         weight = self.weight.view(self.K, -1)
@@ -160,14 +223,14 @@ class Dynamic_LSQConv2d(nn.Module):
 
 
         aggregate_weight = torch.mm(softmax_attention, weight).view(-1, self.in_channels, self.kernel_size, self.kernel_size)
-        aggregate_activation = torch.mm(softmax_attention, _x).sum(0).view(1, -1, height, width)
+        # aggregate_activation = torch.mm(softmax_attention, _x).sum(0).view(1, -1, height, width)
             
         if self.bias is not None:
             aggregate_bias = torch.mm(softmax_attention, self.bias).view(-1)
-            output = F.conv2d(aggregate_activation, weight=aggregate_weight, bias=aggregate_bias, stride=self.stride, padding=self.padding,
+            output = F.conv2d(x, weight=aggregate_weight, bias=aggregate_bias, stride=self.stride, padding=self.padding,
                               dilation=self.dilation, groups=self.groups*batch_size)
         else:
-            output = F.conv2d(aggregate_activation, weight=aggregate_weight, bias=None, stride=self.stride, padding=self.padding,
+            output = F.conv2d(x, weight=aggregate_weight, bias=None, stride=self.stride, padding=self.padding,
                               dilation=self.dilation, groups=self.groups * batch_size)
 
         output = output.view(batch_size, self.out_channels, output.size(-2), output.size(-1))
