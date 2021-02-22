cd6642f1739d896d6969eecd61c4d3705245f357
phuocphn <phuocphn@gmail.com>
2021-02-22 15:44:57 +0900

    correct initialization

--- quantizer/condlsq.py
+++ quantizer/condlsq.py
@@ -132,7 +132,7 @@ class WeightAtentionQuantizer(torch.nn.Module):
 
 class Dynamic_LSQConv2d(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, ratio=0.25, stride=1, padding=0, dilation=1, groups=1, bias=True, K=4,temperature=34, init_weight=True, bit=2):
-        super(Dynamic_LSQConv2d, self).__init__(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1)
+        super(Dynamic_LSQConv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups)
         assert in_channels%groups==0
         self.in_channels = in_channels
         self.out_channels = out_channels
