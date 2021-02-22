41b046432fa7fa0c172ce110a7ba4470d7f6c9c4
phuocphn <phuocphn@gmail.com>
2021-02-22 11:22:00 +0900

    quantize attention module

--- quantizer/condlsq.py
+++ quantizer/condlsq.py
@@ -21,6 +21,62 @@ def gradient_approximation(x, temperature):
     y_grad  = F.softmax(x/temperature, 1)
     return (y- y_grad).detach() + y_grad
 
+
+class LSQQuantizer(torch.nn.Module):
+    def __init__(self, bit, is_activation=False):
+        super(LSQQuantizer,self).__init__()
+
+        self.alpha = nn.Parameter(torch.Tensor(1))
+        self.bit = bit
+        self.is_activation = is_activation
+        self.register_buffer('init_state', torch.zeros(1))        
+        
+        if is_activation:
+            self.Qn = 0
+            self.Qp = 2 ** self.bit - 1
+        else:
+            self.Qn = -2 ** (self.bit - 1)
+            self.Qp = 2 ** (self.bit - 1) - 1
+
+    def forward(self, x):
+        if self.training and self.init_state == 0:
+            self.alpha.data.copy_(2 * x.detach().abs().mean() / math.sqrt(self.Qp))
+            self.init_state.fill_(1)
+            print (self.__class__.__name__, "Initializing step-size value ...")
+        
+        g = 1.0 / math.sqrt(x.numel() * self.Qp)
+        _alpha = grad_scale(self.alpha, g)
+        x_q = round_pass((x / _alpha).clamp(self.Qn, self.Qp)) * _alpha
+        return x_q
+
+    def __repr__(self):
+        return "LSQQuantizer (bit=%s, is_activation=%s)" % (self.bit, self.is_activation)
+
+
+class Conv2dLSQ(nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True, bit=8):
+
+
+        super(Conv2dLSQ, self).__init__(
+            in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+            stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+
+        self.quan_w = LSQQuantizer(bit=bit, is_activation=False)
+        self.quan_a = LSQQuantizer(bit=bit, is_activation=True)
+
+        self.bit = bit
+
+    def forward(self, x):
+        if self.bit == 32:
+            return F.conv2d(x, self.weight, self.bias, self.stride,
+                            self.padding, self.dilation, self.groups)
+        else:
+            return F.conv2d(self.quan_a(x), self.quan_w(self.weight), self.bias, self.stride,
+                            self.padding, self.dilation, self.groups)
+
+
+
 class attention2d(nn.Module):
     def __init__(self, in_channels, ratios, K, temperature, init_weight=True):
         super(attention2d, self).__init__()
@@ -30,9 +86,9 @@ class attention2d(nn.Module):
             hidden_planes = int(in_channels*ratios)+1
         else:
             hidden_planes = K
-        self.fc1 = nn.Conv2d(in_channels, hidden_planes, 1, bias=False)
+        self.fc1 = Conv2dLSQ(in_channels, hidden_planes, 1, bias=False)
         # self.bn = nn.BatchNorm2d(hidden_planes)
-        self.fc2 = nn.Conv2d(hidden_planes, K, 1, bias=True)
+        self.fc2 = Conv2dLSQ(hidden_planes, K, 1, bias=True)
         self.temperature = temperature
         if init_weight:
             self._initialize_weights()
